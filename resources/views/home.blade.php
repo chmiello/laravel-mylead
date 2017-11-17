50fd1886a11de11b3f219b816c81e40981582824
septendev <ziomek@chmiello.pl>
2017-11-17 16:26:06 +0100

    Add link to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a href="{{ route('products.list') }}">Przejdź do zarządzania produktami</a>
                 </div>
             </div>
         </div>
